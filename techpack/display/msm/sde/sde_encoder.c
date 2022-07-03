084d961b39ed858bb0e50c0b925bdc8feeeb13e3
Danny Lin <danny@kdrag0n.dev>
2022-07-03 15:03:37 -0500

    techpack: display: Trigger panel reset after first kickoff
    If we're using a refresh rate different from what the bootloader
    initializes the display with, a panel reset is necessary for our custom
    refresh rate to take effect.
    
    After the first kickoff, request a panel reset from userspace using the
    existing ESD recovery mechanism to update the refresh rate. Note that
    this is only done if the target refresh rate is *not* 60 Hz because this
    is redundant if we don't need a refresh rate update.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Change-Id: I2a4dcae2043d651926929aa422ab34d655c15991

--- techpack/display/msm/sde/sde_encoder.c
+++ techpack/display/msm/sde/sde_encoder.c
@@ -4555,6 +4555,7 @@ static int _sde_encoder_reset_ctl_hw(struct drm_encoder *drm_enc)
 void sde_encoder_kickoff(struct drm_encoder *drm_enc, bool is_error,
 		bool config_changed)
 {
+	static bool first_run = true;
 	struct sde_encoder_virt *sde_enc;
 	struct sde_encoder_phys *phys;
 	unsigned int i;
@@ -4637,6 +4638,25 @@ void sde_encoder_kickoff(struct drm_encoder *drm_enc, bool is_error,
 		if (phys && phys->ops.handle_post_kickoff)
 			phys->ops.handle_post_kickoff(phys);
 	}
+
+	/*
+	 * Trigger a panel reset if this is the first kickoff and the refresh
+	 * rate is not 60 Hz
+	 */
+	if (cmpxchg(&first_run, true, false)) {
+		struct sde_connector *conn = container_of(phys->connector, struct sde_connector, base);
+		struct drm_event event = {
+			.type = DRM_EVENT_PANEL_DEAD,
+			.length = sizeof(bool)
+		};
+
+		conn->panel_dead = true;
+		event.type = DRM_EVENT_PANEL_DEAD;
+		event.length = sizeof(bool);
+		msm_mode_object_event_notify(&conn->base.base,
+			conn->base.dev, &event, (u8 *) &conn->panel_dead);
+	}
+
 	SDE_ATRACE_END("encoder_kickoff");
 #ifdef CONFIG_OPLUS_SYSTEM_CHANGE
 	sde_connector_update_backlight(sde_enc->cur_master->connector, true);
